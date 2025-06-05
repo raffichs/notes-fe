b44e595c2076ddeb7ff1b57bb89dfcbb90c2bc71
raffichaesa <140777811+raffichs@users.noreply.github.com>
2025-06-05 18:15:48 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
